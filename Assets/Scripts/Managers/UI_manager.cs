ff47217d4e2b2c775da5ba3709490c6ee7cd412f
gsantos3 <89170630+gsantos3@users.noreply.github.com>
2024-02-12 19:01:42 -0800

    UI implementation
    Added UI
     text for day, money, time
     Added a system to track how many fishing line you currently have
     (check UI_linecount.cs for details)

---
+++ Assets/Scripts/Managers/UI_manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI_manager : MonoBehaviour
+{
+    public Text Day_text;
+    public Text Money_text;
+    public Text time;
+        
+       
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}

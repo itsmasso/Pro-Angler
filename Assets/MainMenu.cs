5cddf86f8038f15161b26b24cb7f8cf955857a73
Marcilino Lamiy <Maromicho12@gmail.com>
2024-04-12 12:30:41 -0700

    Added Start menu scene

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        sceneManager.LoadSceneAsync();
+    }
+
+}
